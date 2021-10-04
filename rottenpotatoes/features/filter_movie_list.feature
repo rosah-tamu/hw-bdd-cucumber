8839a57b4adde215dfd5de536a5ded25b2e854b9
EC2 Default User <ec2-user@ip-172-31-38-228.us-east-2.compute.internal>
2021-10-04 04:47:38 +0000

    part 2 done

--- rottenpotatoes/features/filter_movie_list.feature
+++ rottenpotatoes/features/filter_movie_list.feature
@@ -23,11 +23,12 @@ Background: movies have been added to database
   Then 10 seed movies should exist
 
 Scenario: restrict to movies with 'PG' or 'R' ratings
-  # enter step(s) to check the 'PG' and 'R' checkboxes
-  # enter step(s) to uncheck all other checkboxes
-  # enter step to "submit" the search form on the homepage
-  # enter step(s) to ensure that PG and R movies are visible
-  # enter step(s) to ensure that other movies are not visible
+  When I check the following ratings: PG, R
+  When I uncheck the following ratings: G, PG-13, NC-17
+  When I press "Refresh" button
+  Then I should see the following movies: Amelie, Raiders of the Lost Ark,  The Incredibles, The Terminator
+  And I should not see the following movies: Chicken Run, Chocolat, The Help
 
 Scenario: all ratings selected
-  # see assignment
+  When I check all movies
+  Then I should see all the movies 
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -26,13 +26,36 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  ratings = rating_list.split(', ')
+  ratings.each do |rating|
+    uncheck ? uncheck("ratings[#{rating}]") : (check("ratings[#{rating}]"))
+  end
+end
+
+When /^I press "(.*)" button/ do |button|
+  click_button button
+end
+
+Then /I should see the following movies: (.*)$/ do |movies_list|
+  movies = movies_list.split(', ')
+  movies.each do |movie|
+    expect(page).to have_content(movie)
+  end
+end
+
+And /I should not see the following movies: (.*)$/ do |movies_list|
+  movies = movies_list.split(', ')
+  movies.each do |movie|
+    expect(page).not_to have_content(movie)
+  end
+end
+
+When(/^I check all movies$/) do
+  Movie.pluck(:rating).uniq.each do |rating| 
+    step %Q{I check "ratings_#{rating}"}
+  end
 end
 
 Then /I should see all the movies/ do
-  # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  expect(page).to have_xpath("//tr", count: 11)
 end
